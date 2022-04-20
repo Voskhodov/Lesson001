d84a61d6f8d7fb270b719e2a9f61bec291ebe991
Alexandr <voskhodov@gmail.com>
2022-04-20 20:19:30 +0500

    Задача_5 решина

--- Zadacha_5/Program.cs
+++ Zadacha_5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int numberA = 782;
+int numberB = (numberA / 10) % 10;
+
+Console.WriteLine (numberA / 10 % 10);
