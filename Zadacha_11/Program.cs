3daca9124656133e86ad6c0d8ff43d65404e3e85
Alexandr <voskhodov@gmail.com>
2022-04-21 21:54:30 +0500

    Задача_11 решина, на вывод кубов введённого числа

---
+++ Zadacha_11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27
+
+Console.Clear();
+Console.Write("Введите число  ");
+string d = Console.ReadLine();
+int a = Convert.ToInt32(d);
+int i = 1;
+while (a > i)
+{
+    int c = i * i;
+    Console.Write($" {c},");
+    i = i + 1;    
+}
+int b = a * a;
+Console.Write($" {b}.");
